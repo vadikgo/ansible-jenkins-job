698935b158e54010df07a4c6677c82317d7447b7
Vladislav Gorbunov <vadikgo@gmail.com>
2017-03-07 23:08:07 +0300

    remove unused lines

--- library/remote_jenkins_job.py
+++ library/remote_jenkins_job.py
@@ -5,11 +5,6 @@ import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-try:
-    import json
-except ImportError:
-    import simplejson as json
-
 DOCUMENTATION = '''
 ---
 module: remote_jenkins_job
